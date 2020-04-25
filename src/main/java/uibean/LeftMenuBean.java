f7bb4c376824745f7e50cb3c620adf61aefcc9bd
Vũ Phương <vuvietphuonggm@gmail.com>
2020-04-25 20:15:21 +0700

    Tao template

---
+++ src/main/java/uibean/LeftMenuBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uibean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.primefaces.component.column.Column;
+import org.primefaces.component.megamenu.MegaMenu;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.Submenu;
+
+/**
+ *
+ * @author vuvie
+ */
+@ManagedBean
+@RequestScoped
+public class LeftMenuBean {
+
+    /**
+     * Creates a new instance of LeftMenuBean
+     */
+    public LeftMenuBean() {
+        
+    }    
+}
